b67c677e37fae13c4ee60b1ba69614e72ae22fa0
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-01-04 21:03:39 +0530

    worked with list functions

--- 2_TODO_LIST/index.js
+++ 2_TODO_LIST/index.js
@@ -20,12 +20,14 @@ let newTask={
     createdOn:Date(),
     createdAt:Date.now()
 }
+let value=newTask.createdOn;
+Edited_value=value.substring(4,16)
+console.log(newTitle + " TODO Successfully added " + Edited_value);
 tasks.push(newTask);
-console.log("TODO Successfully added");
-console.log(tasks);
 // let new_array=[...tasks];
 // new_array.push(newTask);
 }
+
 
 
 function deleteTask(title){
@@ -36,27 +38,73 @@ function deleteTask(title){
     }
     console.log("TODO has been deleted");
     console.log(tasks);
-    
 }
+
+console.log("************************************************************");
+
 
 //Toggles task completeion status
 
 function toggleCompletion(title){
-    console.log("i am here");
     for (let i=0;i<tasks.length;i++){
-        console.log(title);
-        if(tasks[i].title==title){ 
-            value=tasks[i].completed;
-            tasks[i].completed=!value;
-        }   
+        if(tasks[i].title==title){
+            tasks[i].completed=!tasks[i].completed;
+            console.log(tasks[i].title + " has been marked completed");
+        }
+        
     }
 }
 
-addTask("web-03");
+console.log("************************************************************");
 
-toggleCompletion("web-03");
-console.log("after toggling");
-console.log(tasks)
+//Display all task
+
+function listAllTaks(){
+    
+    for (let i=0;i<tasks.length;i++){
+        counter=i+1;
+        console.log("🐢 " + counter + ". " + tasks[i].title);
+    }
+}
+console.log("************************************************************");
+
+// Displaying completed TODO
+function listCompletedTasks(){
+    for(let i=0;i<tasks.length;i++){
+        if(tasks[i].completed==true){
+            console.log("************* Completed Todo's ***********");
+            console.log("✅", tasks[i].title + " has been completed ");
+        }
+        
+    }
+}
+
+console.log("************************************************************");
+
+// Displaying incomplete todo
+
+function listPendingTasks(){
+    
+    for(let i=0;i<tasks.length;i++){
+        if(tasks[i].completed==false){
+        console.log("👀👀", tasks[i].title + " is still pending in yout TODO,S list."); 
+        }
+        
+    }
+}
+
+
+addTask("angular");
+addTask("javascript");
+addTask("minicooper");
+console.log("************************************************************");
+toggleCompletion("angular");
+console.log("************************************************************");
+listAllTaks();
+console.log("************************************************************");
+listCompletedTasks();
+console.log("************************************************************");
+listPendingTasks();
 
 
 
