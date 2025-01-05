a995f2f13f5510c69fe80a42a1d0fd661e9a772f
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-01-05 16:33:27 +0530

    learned about callbacks

---
+++ 3_ASYNC_COUNTER/index.js
@@ -0,0 +1,50 @@
+console.log("I am here");
+
+let global_value;
+
+
+
+//let counter=document.getElementById("counter").innerText;
+
+
+
+
+
+const plus_btn=document.getElementById("plus");
+console.log("i am after plus_btn")
+plus_btn.addEventListener("click",()=>{
+    runtime=document.getElementById("delaytime").value;
+    setTimeout(Increment,runtime);
+    
+});
+
+const minus_btn=document.getElementById("minus");
+    minus_btn.addEventListener("click",()=>{
+    runtime=document.getElementById("delaytime").value;
+    setTimeout(Decrement,runtime);
+    
+})
+
+
+function Increment(){
+    let global_value=document.getElementById("pluscounter").value;
+    let counter=document.getElementById("counter").innerText;
+    console.log(global_value);
+    new_value=parseInt(counter)+parseInt(global_value);
+    console.log(counter)
+    console.log(new_value);
+     document.getElementById("counter").innerText=new_value;
+}
+
+function Decrement(){
+    let global_value =document.getElementById("minuscounter").value;
+    let counter=document.getElementById("counter").innerText;
+    console.log("dec");
+    console.log(global_value);
+    console.log(counter);
+    new_value=parseInt(counter)-parseInt(global_value);
+
+     document.getElementById("counter").innerText=new_value;
+}
+
+
