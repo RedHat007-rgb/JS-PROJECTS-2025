629f603a037e1b15d694f876fd8a7b88d147b853
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-01-11 14:04:01 +0530

    Done with pushingArray

--- Quiz_App/Backend/Exam.js
+++ Quiz_App/Backend/Exam.js
@@ -1,5 +1,48 @@
-
+let StorageArray=[];
 
 export default function startExam(questions){
-    console.log(questions);
+    randomGenerator(questions);
 }
+
+
+
+
+function randomGenerator(questions){
+    while(StorageArray.length<20){
+    let number=Math.floor((Math.random(questions)*questions.length));
+    console.log("randomnumber"+number);
+    pushingArray(questions,number);
+}
+}
+
+function pushingArray(questions,num){
+    if(StorageArray.includes(questions[num].id)==true){
+        console.log("indie includes.........");
+        randomGenerator(questions);
+    }else{
+        console.log("insideelse in push......")
+        StorageArray.push(questions[num].id);
+        console.log(StorageArray);
+        console.log("length............"+ StorageArray.length);
+        console.log(questions[num].question);
+        randomGenerator(questions);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
