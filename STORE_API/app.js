db4d3d90ec67d39294cfafe6c7f8b230caf15373
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-01-19 10:34:07 +0530

    added middlewares

--- STORE_API/app.js
+++ STORE_API/app.js
@@ -5,10 +5,11 @@ const express=require("express");
 const router=require("./routes/tasks.js")
 const app=express();
 const port=3000;
+const notFound=require("./middlewares/notFOund.js")
 app.use("/v1/products",router);
-app.get("/hello",(req,res)=>{
-    res.status(200).send("Hello,setup done......")
-})
+app.use(notFound);
+
+
 const startDB=async()=>{
     await database(process.env.MONGO_URI);
     app.listen(3000,()=>{
---
+++ STORE_API/middlewares/notFOund.js
@@ -0,0 +1,7 @@
+
+const notFound=(req,res)=>{
+    res.status(404).send("Sorry, PLease check the URL and try again....")
+}
+
+
+module.exports=notFound;
