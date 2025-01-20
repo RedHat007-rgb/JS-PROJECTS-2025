b8e5c8794a884f8ea12977ab6ed99cae85434042
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-01-20 13:02:17 +0530

    learned how to count the requests

--- PRACTISE/ASSIGNMENT_MIDDLEWARES/index.js
+++ PRACTISE/ASSIGNMENT_MIDDLEWARES/index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 
+let reqCount=0;
 app.use(express.json());
 function usingMiddleware(req,res,next){
     console.log(req.method);
@@ -8,8 +9,15 @@ function usingMiddleware(req,res,next){
     console.log(Date.now());
     next();
 }
-
 app.use(usingMiddleware);
+function requestCount(req,res,next){
+    reqCount=reqCount+1;
+    console.log("Count: " +reqCount)
+    next();
+}
+app.use(requestCount)
+
+
 
 app.get("/sum/",(req,res)=>{
     let a=parseInt(req.query.a);
