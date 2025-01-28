3a2d269958c8cad9c0fe1b3c3e259f93e52cd29f
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-01-28 17:27:19 +0530

    configures database

--- RECREATING....../app.js
+++ RECREATING....../app.js
@@ -2,12 +2,22 @@ require("dotenv").config();
 const port=process.env.PORT ;
 const express=require("express");
 const router = require("./routes/route");
+const connect = require("./db/connect");
+
 const app=express();
 
 
 app.use(express.json());
 app.use("/",router);
 
-app.listen(port,()=>{
+
+
+
+app.listen(port,async()=>{
+    try{
+    await connect();
     console.log(`Server is listening to ${port}`)
+    }catch(e){
+        console.log(e);
+    }
 })
---
+++ RECREATING....../db/connect.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const mongoose=require("mongoose");
+url=process.env.MONGO_URI;
+const connect=async ()=>{
+    return await mongoose.connect(url);
+   
+}
+
+module.exports=connect;
